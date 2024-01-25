45fe09f1d67c1c0b8c41259ea5762a1dead53b25
Vladimir-Serebrennikov <serebvl23@gmail.com>
2024-01-25 13:04:07 +0300

    project initialization

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    application
+    id("java")
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+application { mainClass.set("hexlet.code.AppApplication") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
