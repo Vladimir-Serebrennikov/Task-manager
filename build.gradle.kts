003d428b851bb5d8f578b0f7620df31ff55c10c2
Vladimir-Serebrennikov <serebvl23@gmail.com>
2024-02-23 13:45:59 +0300

    update Readme file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     application
     id("java")
     id("jacoco")
-    id("checkstyle")
+    //id("checkstyle")
     id("io.freefair.lombok") version "8.4"
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
